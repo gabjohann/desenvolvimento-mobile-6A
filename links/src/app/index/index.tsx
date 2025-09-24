2780958dcf2ff19782bc87209e8589d26e2ceefd
Lucas Johann <gabif.johann@gmail.com>
2025-09-24 18:01:42 -0300

    app links - aula 24/09

---
+++ links/src/app/index/index.tsx
@@ -0,0 +1,28 @@
+import { Categories } from '@/components/categories'
+import { Link } from '@/components/link'
+import { colors } from '@/styles/colors'
+import { MaterialIcons } from '@expo/vector-icons'
+import { Image, TouchableOpacity, View } from 'react-native'
+import { styles } from './styles'
+
+export default function Index() {
+	return (
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<Image source={require('@/assets/logo.png')} style={styles.logo} />
+
+				<TouchableOpacity>
+					<MaterialIcons name="add" size={32} color={colors.green[300]} />
+				</TouchableOpacity>
+			</View>
+
+			<Categories />
+
+			<Link
+				name="Expo"
+				url="https://docs.expo.dev/"
+				onDetails={() => console.log('clicou!')}
+			/>
+		</View>
+	)
+}
---
+++ links/src/app/index/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingTop: 42,
+	},
+	header: {
+		width: '100%',
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		paddingHorizontal: 24,
+		marginBottom: 32,
+	},
+	logo: {
+		width: 32,
+		height: 32,
+	},
+})
