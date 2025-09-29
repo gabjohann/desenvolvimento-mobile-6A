16265daf334e4f80993c405965b1dee7e2ef97be
Lucas Johann <gabif.johann@gmail.com>
2025-09-29 17:47:02 -0300

    app links - aula 29/09

--- links/src/app/index/index.tsx
+++ links/src/app/index/index.tsx
@@ -1,28 +1,68 @@
 import { Categories } from '@/components/categories'
 import { Link } from '@/components/link'
+import { Option } from '@/components/option'
 import { colors } from '@/styles/colors'
+import { categories } from '@/utils/category'
 import { MaterialIcons } from '@expo/vector-icons'
-import { Image, TouchableOpacity, View } from 'react-native'
+import { router } from 'expo-router'
+import { useState } from 'react'
+import {
+	FlatList,
+	Image,
+	Modal,
+	Text,
+	TouchableOpacity,
+	View,
+} from 'react-native'
 import { styles } from './styles'
 
 export default function Index() {
+	const [category, setCategory] = useState(categories[0].name)
+
 	return (
 		<View style={styles.container}>
 			<View style={styles.header}>
 				<Image source={require('@/assets/logo.png')} style={styles.logo} />
 
-				<TouchableOpacity>
+				<TouchableOpacity onPress={() => router.navigate('/add')}>
 					<MaterialIcons name="add" size={32} color={colors.green[300]} />
 				</TouchableOpacity>
 			</View>
 
-			<Categories />
+			<Categories onChange={setCategory} selected={category} />
 
-			<Link
-				name="Expo"
-				url="https://docs.expo.dev/"
-				onDetails={() => console.log('clicou!')}
+			<FlatList
+				data={['1', '2', '3']}
+				keyExtractor={(item) => item}
+				renderItem={({ item }) => (
+					<Link
+						name="Expo"
+						url="https://docs.expo.dev/"
+						onDetails={() => console.log('clicou!')}
+					/>
+				)}
+				style={styles.links}
+				contentContainerStyle={styles.linksContent}
+				showsVerticalScrollIndicator={false}
 			/>
+
+			<Modal transparent visible={false}>
+				<View style={styles.modal}>
+					<View style={styles.modalContent}>
+						<View style={styles.modalHeader}>
+							<Text style={styles.modalCategory}>Curso</Text>
+							<MaterialIcons name="close" size={20} color={colors.gray[400]} />
+						</View>
+						<Text style={styles.modalLinkName}>Google</Text>
+						<Text style={styles.modalUrl}>https://google.com</Text>
+
+						<View style={styles.modalFooter}>
+							<Option name="Excluir" icon="delete" variant="secondary" />
+							<Option name="Abrir" icon="language" />
+						</View>
+					</View>
+				</View>
+			</Modal>
 		</View>
 	)
 }
--- links/src/app/index/styles.ts
+++ links/src/app/index/styles.ts
@@ -1,3 +1,4 @@
+import { colors } from '@/styles/colors'
 import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
@@ -16,5 +17,55 @@ export const styles = StyleSheet.create({
 	logo: {
 		width: 32,
 		height: 32,
+	},
+	links: {
+		borderTopWidth: 1,
+		borderTopColor: colors.gray[600],
+	},
+	linksContent: {
+		gap: 20,
+		padding: 20,
+		paddingBottom: 100,
+	},
+	modal: {
+		flex: 1,
+		justifyContent: 'flex-end',
+	},
+	modalContent: {
+		backgroundColor: colors.gray[900],
+		borderTopWidth: 1,
+		borderTopColor: colors.gray[800],
+		padding: 24,
+		paddingBottom: 32,
+	},
+	modalHeader: {
+		width: '100%',
+		flexDirection: 'row',
+		alignItems: 'center',
+		marginBottom: 32,
+	},
+	modalCategory: {
+		flex: 1,
+		fontSize: 16,
+		fontWeight: '500',
+		color: colors.gray[400],
+	},
+	modalLinkName: {
+		fontSize: 18,
+		fontWeight: '600',
+		color: colors.gray[200],
+	},
+	modalUrl: {
+		fontSize: 14,
+		color: colors.gray[400],
+	},
+	modalFooter: {
+		width: '100%',
+		marginTop: 32,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		borderTopWidth: 1,
+		borderTopColor: colors.gray[600],
+		paddingVertical: 14,
 	},
 })
